3468d425fd2c0cfc906eb2111a006cc2dd358504
bluberry-dream <khan.ssjc@gmail.com>
2024-06-21 11:45:57 -0400

    feat: intro to debugging

--- java-fundamentals/1_hello_world/HelloWorld.java
+++ java-fundamentals/1_hello_world/HelloWorld.java
@@ -1,6 +1,19 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String greeting = "Hello, World!";
+
+        System.out.println(greeting);
+        
+        int number = 5;
+        int doubled = doubleNumber(number);
+
+        System.out.println("The doubled number is:\t" + doubled);
+
+        return;
+    }
+
+    public static int doubleNumber(int num) {
+        return num * 2;
     }
 }
